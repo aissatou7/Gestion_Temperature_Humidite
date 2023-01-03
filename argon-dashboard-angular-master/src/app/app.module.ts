28259d4c9712e7abe725e0ed458f8adef2f76d8e
kadidia <diallokhadidiatoubouba@gmail.com>
2023-01-03 13:13:35 +0000

    ok

--- argon-dashboard-angular-master/src/app/app.module.ts
+++ argon-dashboard-angular-master/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
@@ -22,7 +22,9 @@ import { ComponentsModule } from './components/components.module';
     ComponentsModule,
     NgbModule,
     RouterModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+
   ],
   declarations: [
     AppComponent,
